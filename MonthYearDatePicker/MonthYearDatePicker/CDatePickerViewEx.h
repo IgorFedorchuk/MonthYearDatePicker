beaa01d3a5398f7cf419ab22086a5275a5bd27fe
Igor Fedorchuk <ifedorchuk@neoline.biz>
2013-03-18 09:08:13 +0200

    Add CDatePickerViewEx class

---
+++ MonthYearDatePicker/MonthYearDatePicker/CDatePickerViewEx.h
@@ -0,0 +1,14 @@
+//
+//  CDatePickerViewEx.h
+//  MonthYearDatePicker
+//
+//  Created by Igor on 18.03.13.
+//  Copyright (c) 2013 Igor. All rights reserved.
+//
+
+@interface CDatePickerViewEx : UIPickerView <UIPickerViewDelegate, UIPickerViewDataSource>
+
+@property (nonatomic, strong, readonly) NSDate *date;
+-(void)selectToday;
+
+@end
