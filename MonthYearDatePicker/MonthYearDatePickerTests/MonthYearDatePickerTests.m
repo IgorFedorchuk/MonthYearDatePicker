95456a4495305b77f4eb98accbe19de5607a7df2
Igor Fedorchuk <ifedorchuk@neoline.biz>
2013-03-18 08:58:11 +0200

    Initial commit

---
+++ MonthYearDatePicker/MonthYearDatePickerTests/MonthYearDatePickerTests.m
@@ -0,0 +1,32 @@
+//
+//  MonthYearDatePickerTests.m
+//  MonthYearDatePickerTests
+//
+//  Created by Igor on 18.03.13.
+//  Copyright (c) 2013 Igor. All rights reserved.
+//
+
+#import "MonthYearDatePickerTests.h"
+
+@implementation MonthYearDatePickerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MonthYearDatePickerTests");
+}
+
+@end
